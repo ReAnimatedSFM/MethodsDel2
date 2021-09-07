3b4ad4367ccd918694d924903bba3eb932b99e6d
William Johannes Saaby Porsmose <will5606@edu.campusvejle.dk>
2021-09-07 10:41:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDel2
+{
+    class Program
+    {
+        /*
+        private static int[] Generate(int a) //1 - 100
+        {
+            Random rndNumber = new Random();
+            int[] heltTalsArray = new int[a];
+
+            for (int i = 0; i < heltTalsArray.Length; i++)
+            {
+                heltTalsArray[i] = rndNumber.Next(0, 101);
+            }
+
+            return heltTalsArray;
+        }
+        */
+
+        
+        private static int[] Generate(int a) // -int32 til +int32
+        {
+            Random rndNumber = new Random();
+            int[] heltTalsArray = new int[a];
+
+            for (int i = 0; i < heltTalsArray.Length; i++)
+            {
+                heltTalsArray[i] = rndNumber.Next();
+            }
+
+            return heltTalsArray;
+        }
+        
+
+        private static void Print(int[] tempArray)
+        {
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                Console.WriteLine("Index" + i +  ": " + tempArray[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = Generate(10);
+            Print(a);
+            Console.ReadKey();
+        }
+    }
+}
